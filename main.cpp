bf6aa007e555379095da41e89b58a905b43dba5b
Clint <cdunn6754@gmail.com>
2020-07-12 17:42:15 -0600

    Initial ppgql repel

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstddef>
+#include <memory>
+#include <stdlib.h>
+
+struct InputBuffer
+{
+  std::string buffer;
+  std::size_t buffer_size;
+  std::size_t input_length;
+};
+
+void print_prompt() {
+  std::cout << "db_name > ";
+}
+
+void read_input(std::unique_ptr<InputBuffer>& ib) {
+  std::getline(std::cin, ib->buffer);
+}
+
+void process_input(std::string& buffer) {
+  std::cout << "Got it: " << buffer << std::endl;  
+}
+
+int main(int argc, char* argv[]) {
+  std::unique_ptr<InputBuffer> ib (new InputBuffer);
+  while (true) {
+    print_prompt();
+    read_input(ib);
+    
+    if (ib->buffer.compare("\\c") == 0 || ib->buffer.compare("q") == 0) {
+      std::exit(EXIT_SUCCESS);
+    }
+
+    process_input(ib->buffer);
+  }
+}
